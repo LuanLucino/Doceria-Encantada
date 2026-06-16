cdc6aa4b9a1c35a2c9c644ac172b0cb14b31ebb5
Luan Lucino <109971413+LuanLucino@users.noreply.github.com>
2026-06-16 17:29:06 -0300

    Sistema de Login melhorado

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -12,12 +12,13 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   const firebaseConfig = {
-    apiKey: "AIzaSyAvffw23q6XvrWUlChp76rrTytjgMXpL3VE",
+    apiKey: "AIzaSyAvffw23qX6rWU1cHp76rrTytgjMXpl3VE",
     authDomain: "doceria-encantada.firebaseapp.com",
     projectId: "doceria-encantada",
-    storageBucket: "doceria-encantada.appspot.com",
-    messagingSenderId: "588485252022",
-    appId: "1:588485252022:web:304910a55cb2b71ed9f0bb"
+    storageBucket: "doceria-encantada.firebasestorage.app",
+    messagingSenderId: "588458252002",
+    appId: "1:588458252002:web:304910a55cb2b71ed9f0bb",
+    measurementId: "G-K15XZNXBE9"
   };
 
   const app  = initializeApp(firebaseConfig);
